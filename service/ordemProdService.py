28d5c762932a67046f74c3b872e7f04e588ee7ee
luis-fe <luisengp@gmail.com>
2023-11-21 14:01:17 -0300

    Acrescentando consulta de OrdemProducao

--- service/ordemProdService.py
+++ service/ordemProdService.py
@@ -4,21 +4,46 @@ import ConexaoCSW
 
 def OrdemProd(empresa):
     conn = ConexaoCSW.Conexao()
+    empresa = "'"+empresa+"'"
     consulta = pd.read_sql('SELECT codLote, numeroOP, codProduto, codTipoOP,  codFaseAtual '
                           ' from tco.OrdemProd op '
-                          " WHERE op.codEmpresa = 1 and "
+                          " WHERE op.codEmpresa = "+ empresa+
+                           " and "
                            " (op.codLote like '23%' or op.codLote like '24%'  ) "
                            "and situacao in (2, 3)",conn)
 
     consulta2 = pd.read_sql('SELECT codLote, numeroOP, codProduto, codTipoOP,  codFaseAtual '
                           ' from tco.OrdemProd op '
-                          " WHERE op.codEmpresa = 1 and "
+                                                    " WHERE op.codEmpresa = "+ empresa+
+                            " and "
                            " (op.codLote not like '23%' and op.codLote not like '24%'  ) "
                            "and situacao in (3)",conn)
 
     consulta = pd.concat([consulta, consulta2], ignore_index=True)
+    consulta.fillna('-', inplace=True)
 
     conn.close()
     return consulta
 
-print(OrdemProd('1'))
+def RoteiroOP(dataframeLOTE):
+
+    dataframeLOTE = dataframeLOTE[['codLote']]
+    dataframeLOTE = dataframeLOTE.drop_duplicates(subset=['codLote'])
+
+    # Passo 3: Transformar o dataFrame em lista
+    resultado = '({})'.format(', '.join(["'{}'".format(valor) for valor in dataframeLOTE['codLote']]))
+
+    conn = ConexaoCSW.Conexao()
+    consulta = pd.read_sql('SELECT r.numeroOP , r.codSeqRoteiro , r.codFase FROM tco.RoteiroOP r '
+                           'WHERE r.codempresa = 1 and  '
+                           'r.codLote  in '+ resultado,conn)
+    conn.close()
+    return consulta
+
+
+def ConjuntodeOP(empresa):
+
+    ordemprod = OrdemProd(empresa)
+    roteiro = RoteiroOP(ordemprod)
+    conjunto = pd.merge(ordemprod,roteiro,on='numeroOP')
+    print(conjunto)
