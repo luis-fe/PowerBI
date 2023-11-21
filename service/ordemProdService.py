71d107649a0e9ff22a06206b39d88be96eaaf055
luis-fe <luisengp@gmail.com>
2023-11-21 12:37:18 -0300

    Acrescentando consulta de OrdemProducao

--- service/ordemProdService.py
+++ service/ordemProdService.py
@@ -1,4 +1,3 @@
-import pandas
 import pandas as pd
 
 import ConexaoCSW
@@ -7,7 +6,18 @@ def OrdemProd(empresa):
     conn = ConexaoCSW.Conexao()
     consulta = pd.read_sql('SELECT codLote, numeroOP, codProduto, codTipoOP,  codFaseAtual '
                           ' from tco.OrdemProd op '
-                          " WHERE op.codEmpresa = 1 and op.codLote like '23%' and situacao in (2, 3)",conn)
+                          " WHERE op.codEmpresa = 1 and "
+                           " (op.codLote like '23%' or op.codLote like '24%'  ) "
+                           "and situacao in (2, 3)",conn)
+
+    consulta2 = pd.read_sql('SELECT codLote, numeroOP, codProduto, codTipoOP,  codFaseAtual '
+                          ' from tco.OrdemProd op '
+                          " WHERE op.codEmpresa = 1 and "
+                           " (op.codLote not like '23%' and op.codLote not like '24%'  ) "
+                           "and situacao in (3)",conn)
+
+    consulta = pd.concat([consulta, consulta2], ignore_index=True)
+
     conn.close()
     return consulta
 
