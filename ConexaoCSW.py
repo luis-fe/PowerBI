17710ffc0f668adce4d0b7e0fcc00efbe4252ad0
luis-fe <luisengp@gmail.com>
2023-12-12 15:04:19 -0300

    COMMIT

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -2,6 +2,6 @@ import pyodbc
 
 def Conexao():
     #Conectado ao Banco DE DADOS do ERP DA EMPRESA VIA ODBC
-    conn = pyodbc.connect(dsn = 'CSW SISTEMAS', user ='root', password = 'ccscache')
+    conn = pyodbc.connect(dsn = 'CONSISTEM', user ='root', password = 'ccscache')
 
     return conn
